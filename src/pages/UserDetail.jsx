9ef0e1f4481706c035e3a6fdb9317c0ed3f1cc13
Sudip Ghosh <sudip02ghosh@gmail.com>
2024-08-06 15:01:16 +0530

    user detail

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
-
+import { Link, useLocation, useParams } from "react-router-dom";
+import person from "../assets/person.png";
+import Sidebar from "@/components/Sidebar";
+import { IoMdArrowBack } from "react-icons/io";
 const UserDetail = () => {
   const location = useLocation();
   const { user } = location.state;
 
   //   console.log(user);
   return (
-    <div>
-      {user ? (
-        <div>
-          <h1>{user.fullName}</h1>
-          <p>Country: {user.country}</p>
-          <p>Type: {user.type}</p>
-          <p>Last Active: {user.lastTimeActive}</p>
-          <p>Sessions: {user?.sessions}</p>
+    <div className="h-[calc(100vh-80px)] flex">
+      <Sidebar />
+      <div className=" ml-[32px] mt-[32px]">
+        <Link to="/overview">
+          <div className="w-[92px] h-[40px] flex justify-center items-center bg-[#fff] my-3 gap-2 rounded-[8px]">
+            <IoMdArrowBack />
+            <p>Back</p>
+          </div>
+        </Link>
+        <div className="flex bg-white rounded-[8px] p-[20px]">
+          <img
+            src={person}
+            alt=""
+            className="w-[80px] h-[80px] rounded-[50%] border"
+          />
+          <div className="border" />
+          <div>
+            <h1 className="text-[24px] text-darkblue font-[700] ">
+              {user?.fullName}
+            </h1>
+          </div>
         </div>
-      ) : (
-        <p>User not found</p>
-      )}
+      </div>
     </div>
   );
 };
